edaa792b44bdcf21b99e97e5ed761f2fb8cb3f54
weilong <weilong666@foxmail.com>
2025-08-01 17:54:37 +0800

    优化遮罩样式

--- minify/minify.js
+++ minify/minify.js
@@ -1,34 +1,45 @@
 import path from "path";
-import { fileURLToPath } from "url";
+import {
+    fileURLToPath
+} from "url";
 import minify from "@node-minify/core";
 import terser from "@node-minify/terser";
 import cleanCSS from "@node-minify/clean-css";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = path.dirname(__filename);
 const rootPath = path.resolve(__dirname, "../");
 
 async function doMinify() {
     await minify({
-        compressor: terser,
-        input: path.join(rootPath, "data/src/*.js").replace(/\\/g, '/'),
-        output: path.join(rootPath, "data/emulator.min.js"),
-    })
+            compressor: terser,
+            input: path.join(rootPath, "data/src/*.js").replace(/\\/g, '/'),
+            output: path.join(rootPath, "data/emulator.min.js"),
+        })
         .catch(function (err) {
             console.error(err);
         })
-        .then(function() {
+        .then(function () {
             console.log("Minified JS");
         });
+
+    // 生成两个独立的 glob 模式
+    const styleCss = path.join(rootPath, "data/css/**/*.css").replace(/\\/g, '/');
+    const emulatorCss = path.join(rootPath, "data/emulator.css");
+
+    // 合并：将它们放入一个数组
+    const cssGlobs = [emulatorCss, styleCss];
+    
     await minify({
-        compressor: cleanCSS,
-        input: path.join(rootPath, "data/emulator.css"),
-        output: path.join(rootPath, "data/emulator.min.css"),
-    })
+            compressor: cleanCSS,
+            input: cssGlobs,
+            output: path.join(rootPath, "data/emulator.min.css"),
+        })
         .catch(function (err) {
             console.error(err);
         })
-        .then(function() {
+        .then(function () {
             console.log("Minified CSS");
         });
 }
